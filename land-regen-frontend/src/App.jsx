bd3a72d84e22bdacfc409fa221724e42090c618b
Musirmoses <140765140+Musirmoses@users.noreply.github.com>
2025-10-14 10:02:43 +0300

    Update App.jsx

--- land-regen-frontend/src/App.jsx
+++ land-regen-frontend/src/App.jsx
@@ -1,19 +1,18 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Dashboard from "./pages/Dashboard";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
 import AddTree from "./pages/AddTree";
 import Adopt from "./pages/Adopt";
+import Dashboard from "./pages/Dashboard";
 
-function App() {
+export default function App() {
   return (
-    <Router>
+    <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Dashboard />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/plant" element={<AddTree />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/add" element={<AddTree />} />
         <Route path="/adopt" element={<Adopt />} />
+        <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
-    </Router>
+    </BrowserRouter>
   );
 }
-
-export default App;
